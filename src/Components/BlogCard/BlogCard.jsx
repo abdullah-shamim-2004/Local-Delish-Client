d435d85a6f44944366d4f7c7c9cb943001656162
abdullah-shamim-2004 <abdullahshamim884@gmail.com>
2026-02-20 00:15:13 +0600

    Created Blog page and add fuction to fetch blog data.

---
+++ src/Components/BlogCard/BlogCard.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const BlogCard = ({ blog }) => {
+  console.log(blog);
+
+  return <div>hi</div>;
+};
+
+export default BlogCard;
--- src/Pages/Blog/Blog.jsx
+++ src/Pages/Blog/Blog.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import BlogCard from "../../Components/BlogCard/BlogCard";
 
 const Blog = () => {
-  return <div>Blog</div>;
+  const [BlogData, setBlogData] = useState([]);
+
+  useEffect(() => {
+    const fethData = async () => {
+      try {
+        const response = await fetch("/public/blogData.json");
+        const data = await response.json();
+        setBlogData(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fethData();
+  }, []);
+  return (
+    <div className="text-center">
+      <h1 className="text-2xl text-secondary font-semibold">Blog Collection</h1>
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-3">
+        {BlogData?.map((blog) => (
+          <BlogCard key={blog.id} blog={blog}></BlogCard>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Blog;
